ef82a7ce1cffa56b598641c106aa724d8c9e48ed
macraves <macius.caner@gmail.com>
2024-05-25 18:27:24 +0100

    add docstring

--- watermark/watermark_funcs.py
+++ watermark/watermark_funcs.py
@@ -8,7 +8,14 @@ base_dir = os.path.dirname(__file__)
 
 
 def remove_white_background(img: Image):
-    """docs"""
+    """Remove white background from the image.
+
+    Args:
+        img (PIL.Image.Image): The image to remove the white background from.
+
+    Returns:
+        PIL.Image.Image: The image with the white background removed.
+    """
     new_data = []
     data = img.getdata()
     for item in data:
@@ -21,7 +28,16 @@ def remove_white_background(img: Image):
 
 
 def image_opacity(main_img: Image, img: Image, ratio: int = 50):
-    """docs"""
+    """Adjust the opacity of an image and paste it onto another image.
+
+    Args:
+        main_img (PIL.Image.Image): The main image to paste the transparent image onto.
+        img (PIL.Image.Image): The transparent image to adjust the opacity of and paste onto the main image.
+        ratio (int, optional): The opacity ratio of the transparent image. Defaults to 50.
+
+    Returns:
+        PIL.Image.Image: The main image with the transparent image pasted on it.
+    """
     if img.mode != "RGBA":
         alpha = Image.new("L", img.size, 255)
         img.putalpha(alpha)
@@ -36,7 +52,15 @@ def image_opacity(main_img: Image, img: Image, ratio: int = 50):
 
 
 def crop_and_resize(img_path: str, cor=(150, 120, 500, 450)):
-    """docs"""
+    """Crop and resize an image.
+
+    Args:
+        img_path (str): The path to the image file.
+        cor (tuple, optional): The coordinates to crop the image. Defaults to (150, 120, 500, 450).
+
+    Returns:
+        PIL.Image.Image: The cropped and resized image.
+    """
     watermark_img = Image.open(img_path)
     cropped = watermark_img.crop(cor)
     cropped_size = cropped.size
@@ -46,7 +70,15 @@ def crop_and_resize(img_path: str, cor=(150, 120, 500, 450)):
 
 
 def text_watermark(photo_path: str, text: str):
-    """docs"""
+    """Add a text watermark to an image.
+
+    Args:
+        photo_path (str): The path to the image file.
+        text (str): The text to add as a watermark.
+
+    Returns:
+        PIL.Image.Image: The image with the text watermark added.
+    """
     with Image.open(photo_path).convert("RGBA") as base:
         # make a blank image for the text, initialized to transparent text color
         txt = Image.new("RGBA", base.size, (255, 255, 255, 0))
@@ -66,21 +98,36 @@ def text_watermark(photo_path: str, text: str):
 
 
 def save_as(image: Image, new_file_name: str):
-    """Saves the image.
+    """Save the image with a new file name.
+
     Args:
         image (PIL.Image.Image): The image to save.
+        new_file_name (str): The new file name for the image.
     """
     new_file_path = os.path.join(base_dir, new_file_name)
     image.save(new_file_path)
 
 
 def save_image(image: Image, image_file_path: str):
-    """docs"""
+    """Save the image to a specified file path.
+
+    Args:
+        image (PIL.Image.Image): The image to save.
+        image_file_path (str): The file path to save the image to.
+    """
     image.save(image_file_path)
 
 
 def paste_watermark(main_path, watermark_path):
-    """docs"""
+    """Paste a watermark onto the main image.
+
+    Args:
+        main_path (str): The path to the main image file.
+        watermark_path (str): The path to the watermark image file.
+
+    Returns:
+        str: The path to the watermarked image file.
+    """
     watermark = crop_and_resize(watermark_path)
     main_photo = Image.open(main_path)
     main_image = image_opacity(main_img=main_photo, img=watermark)
