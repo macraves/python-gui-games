c76edaecf043f5bd233b868ef6eee2446e96c2d0
macraves <macius.caner@gmail.com>
2024-05-26 23:48:05 +0100

    consider list approach

--- typing_test/desk_typing_test.py
+++ typing_test/desk_typing_test.py
@@ -7,9 +7,13 @@ from all_keysyms import all_keysyms
 letters = list(string.ascii_lowercase + string.ascii_uppercase + " ")
 BG_COLOR = "#ea6e28"
 ENTRY_DEFAULT_TEXT = "Enter the text here"
-PARAGRAPH = "This is a sample paragraph for the typing speed application."
+PARAGRAPH = (
+    "This is a sample paragraph for the typing speed application "
+    "This is a sample paragraph for the typing speed application "
+)
+LST = PARAGRAPH.split()
 FONT_TEXT = ("Arial", 14)
-COUNT = {"count": 0}
+counters = {"count": 0, "index": 0}
 stack = []
 
 
@@ -22,7 +26,7 @@ class TypingSpeedApp:
         self.root.geometry("600x300")
         self.root.config(bg=BG_COLOR, padx=10, pady=10)
 
-        self.paragraph = paragraph
+        self.paragraph = " ".join(LST)
         self.start_time = None
         self.end_time = None
 
@@ -67,27 +71,33 @@ class TypingSpeedApp:
     #         self.entry_input.delete(0, tk.END)
     #         self.entry_input.config(fg="black")
 
-    def change_word_color(self, word: str, is_true: bool, reset=False):
+    def change_leter_color(self, word: str, is_true: bool, reset=False):
         """docs"""
 
-    def track_paragraph(self, count, char):
+    def track_word_character(self, count, letter):
         """docs"""
+        index = counters["index"]
+        word = LST[index]
+        if letter == word[count]:
+            pass
 
     def update_input(self, event):
         """docs"""
 
-        count = COUNT["count"]
+        count = counters["count"]
         char = event.keysym
         if char in ("Shift_L", "Shift_R") and len(stack) == 0:
             stack.append(str.upper)
+        if char == "BackSpace":
+            counters["count"] -= 1
+            counters["count"] = max(counters["count"], 0)
         if char not in all_keysyms:
             if stack:
                 method = stack.pop()
                 char = method(char)
-            # word = self.entry_input.get()
-            # self.track_paragraph(count=COUNT, char=word)
+            self.track_word_character(count=count, letter=char)
             print(char)
-            COUNT["count"] += 1
+            counters["count"] += 1
 
 
 window = tk.Tk()
