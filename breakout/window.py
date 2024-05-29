363a41403cbdd722d0d2f3637d4bfa3d268bd83b
macraves <macius.caner@gmail.com>
2024-05-29 23:29:53 +0100

    border collision proximity set 20

--- breakout/window.py
+++ breakout/window.py
@@ -2,7 +2,7 @@
 
 import time
 from turtle import Screen
-from ball_methods import Ball
+from ball_methods import Ball, Turtle
 from show_border import Border
 
 
@@ -11,6 +11,8 @@ SCREEN_HEIGHT = 600
 PAD_UPPER, PAD_BOTTOM = 10, 25
 PAD_SIDES = 10
 
+X_PROXIMITY = 20
+Y_PROXIMITY = 20
 
 UPPRER_BOUNDRY = (SCREEN_HEIGHT // 2) - PAD_UPPER
 BOTTOM_BOUNDRY = (SCREEN_HEIGHT // 2) - PAD_BOTTOM
@@ -36,15 +38,32 @@ def draw_borders():
 
 draw_borders()
 
+
+def new_ball(current_ball: Turtle):
+    """docs"""
+    x_cor, y_cor = current_ball.xcor(), current_ball.ycor()
+    the_ball = Ball()
+    the_ball.goto(x_cor, y_cor)
+
+
 GAME_ON = True
 while GAME_ON:
+    ball.speed = 0.01
     time.sleep(ball.ball_speed)
     SCREEN.update()
     ball.move()
     # Sides Collision
-    if ball.xcor() > SIDES_BOUNDRY or ball.xcor() < -SIDES_BOUNDRY:
+    if (
+        ball.xcor() > SIDES_BOUNDRY - X_PROXIMITY
+        or ball.xcor() < -SIDES_BOUNDRY + X_PROXIMITY
+    ):
+        new_ball(ball)
         ball.bounce_x()
-    if ball.ycor() > UPPRER_BOUNDRY or ball.ycor() < -BOTTOM_BOUNDRY:
+    if (
+        ball.ycor() > UPPRER_BOUNDRY - Y_PROXIMITY
+        or ball.ycor() < -BOTTOM_BOUNDRY + Y_PROXIMITY
+    ):
+        new_ball(ball)
         ball.bounce_y()
 
 
